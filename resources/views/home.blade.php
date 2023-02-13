70de595dd0ebb3eb54f5984c98776ffe2ebca031
francescotrudu <francescotrudu84@gmail.com>
2023-02-13 20:14:12 +0100

    Add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('main')
+<section id="menu">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12">
+                <h6>Table <a class="text-decoration-none" href="{{route('comics.index')}}">comics</a></h6>
+            </div>
+        </div>
+    </div>
+
+</section>
+@endsection

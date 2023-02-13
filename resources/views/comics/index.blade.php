1f92a561bc883fd39cba5b438854a3f4d038a85b
francescotrudu <francescotrudu84@gmail.com>
2023-02-13 22:16:46 +0100

    Comment columns

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,36 +7,42 @@
         
         <div class="row w-100">
             <div class="col-12">
-                <table class="table table-striped">
+                <table class="table table-striped table-hover table-success">
                     <thead>
                         <tr>
                         <th scope="col">Id</th>
                         <th scope="col">Title</th>
-                        <th scope="col">Description</th>
+                        {{-- <th scope="col">Description</th> --}}
                         <th scope="col">Thumb</th>
                         <th scope="col">Price</th>
                         <th scope="col">Serie</th>
-                        <th scope="col">Sale date</th>
+                        {{-- <th scope="col">Sale date</th> --}}
                         <th scope="col">Type</th>
-                        <th scope="col">Creation date</th>
-                        <th scope="col">Update date</th>
+                        {{-- <th scope="col">Creation date</th> --}}
+                        {{-- <th scope="col">Update date</th> --}}
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($comics as $item)
+                        @forelse ($comics as $item)
                             <tr>
                                 <th scope="row"> {{$item->id}} </th>
                                 <td> {{$item->title}} </td>
-                                <td class="text-truncate" style="max-width: 300px;"> {{$item->description}} </td>
+                                {{-- <td class="text-truncate" style="max-width: 300px;"> {{$item->description}} </td> --}}
                                 <td class="text-truncate" style="max-width: 300px;"> {{$item->thumb}} </td>
                                 <td> {{$item->price}} </td>
                                 <td> {{$item->series}} </td>
-                                <td> {{$item->sale_date}} </td>
+                                {{-- <td> {{$item->sale_date}} </td> --}}
                                 <td> {{$item->type}} </td>
-                                <td> {{$item->created_at}} </td>
-                                <td> {{$item->updated_at}} </td>
+                                {{-- <td> {{$item->created_at}} </td> --}}
+                                {{-- <td> {{$item->updated_at}} </td> --}}
                             </tr>
-                        @endforeach
+                        @empty
+                        <table>
+                            <tr>
+                                <td id="empty" colspan="6"> Empty database </td>
+                            </tr>
+                        </table>
+                        @endforelse
                     </tbody>
                 </table>
             </div>

237fdf482aa1729e41bb732571f550b803b90a31
francescotrudu <francescotrudu84@gmail.com>
2023-02-13 20:15:58 +0100

    Insert data on table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,22 +1,47 @@
 
+@extends('layouts.app')
+
+@section('main')
 <section id="cards">
-    <div class="container">
+    <div class="">
         
-        <div class="row">
-            @foreach ($comics as $comic)
-            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
-                <div class="card-group">
-                    <div class="card">
-                        <img src="{{ $comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
-                        <div class="card-body">
-                            <h6 class="card-title"> {{$comic['title']}} </h6>
-                        </div>
-                    </div>
-                </div>
+        <div class="row w-100">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Thumb</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Serie</th>
+                        <th scope="col">Sale date</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Creation date</th>
+                        <th scope="col">Update date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comics as $item)
+                            <tr>
+                                <th scope="row"> {{$item->id}} </th>
+                                <td> {{$item->title}} </td>
+                                <td class="text-truncate" style="max-width: 300px;"> {{$item->description}} </td>
+                                <td class="text-truncate" style="max-width: 300px;"> {{$item->thumb}} </td>
+                                <td> {{$item->price}} </td>
+                                <td> {{$item->series}} </td>
+                                <td> {{$item->sale_date}} </td>
+                                <td> {{$item->type}} </td>
+                                <td> {{$item->created_at}} </td>
+                                <td> {{$item->updated_at}} </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-            @endforeach
         </div>
     </div>
 
 </section>
-    
+@endsection

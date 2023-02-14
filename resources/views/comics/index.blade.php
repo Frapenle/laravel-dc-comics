8598feb7603400fc42c13e45ad44daae78145872
francescotrudu <francescotrudu84@gmail.com>
2023-02-14 09:28:56 +0100

    edit item in comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -20,21 +20,27 @@
                         <th scope="col">Type</th>
                         {{-- <th scope="col">Creation date</th> --}}
                         {{-- <th scope="col">Update date</th> --}}
+                        <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($comics as $item)
+                        @forelse ($comics as $comic)
                             <tr>
-                                <th scope="row"> {{$item->id}} </th>
-                                <td> {{$item->title}} </td>
-                                {{-- <td class="text-truncate" style="max-width: 300px;"> {{$item->description}} </td> --}}
-                                <td class="text-truncate" style="max-width: 300px;"> {{$item->thumb}} </td>
-                                <td> {{$item->price}} </td>
-                                <td> {{$item->series}} </td>
-                                {{-- <td> {{$item->sale_date}} </td> --}}
-                                <td> {{$item->type}} </td>
-                                {{-- <td> {{$item->created_at}} </td> --}}
-                                {{-- <td> {{$item->updated_at}} </td> --}}
+                                <th scope="row"> {{$comic->id}} </th>
+                                <td> {{$comic->title}} </td>
+                                {{-- <td class="text-truncate" style="max-width: 300px;"> {{$comic->description}} </td> --}}
+                                <td class="text-truncate" style="max-width: 300px;"> {{$comic->thumb}} </td>
+                                <td> {{$comic->price}} </td>
+                                <td style="max-width: 200px;"> {{$comic->series}} </td>
+                                {{-- <td> {{$comic->sale_date}} </td> --}}
+                                <td> {{$comic->type}} </td>
+                                {{-- <td> {{$comic->created_at}} </td> --}}
+                                {{-- <td> {{$comic->updated_at}} </td> --}}
+                                <td style="width: 300px;">
+                                    <button class="btn btn-success bttn-xs">Show</button>
+                                    <button class="btn btn-warning bttn-xs">Edit</button>
+                                    <button class="btn btn-danger bttn-xs">Delete</button>
+                                </td>
                             </tr>
                         @empty
                         <table>

b554c83db43d75b73a22188d5af88e5ed2c2410f
francescotrudu <francescotrudu84@gmail.com>
2023-02-14 13:06:24 +0100

    Show single comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,19 +1,22 @@
 
 @extends('layouts.app')
 
+
 @section('main')
-<section id="cards">
+<section id="cards" class="m-4">
     <div class="">
-        
         <div class="row w-100">
             <div class="col-12">
-                
-                <p scope="row"> {{$comic->id}} </p>
-                <p> {{$comic->title}} </p>
-
-                <button class="btn btn-success bttn-xs">Show</button>
-                <button class="btn btn-warning bttn-xs">Edit</button>
-                <button class="btn btn-danger bttn-xs">Delete</button>
+                <p> <span class="fw-bold">ID:</span> {{$comic->id}} </p>
+                <p> <span class="fw-bold">Title:</span> {{$comic->title}} </p>
+                <p> <span class="fw-bold">Description:</span> {{$comic->description}} </p>
+                <p> <span class="fw-bold">Thumb:</span> {{$comic->thumb}} </p>
+                <p> <span class="fw-bold">Price:</span> {{$comic->price}} </p>
+                <p> <span class="fw-bold">Series:</span> {{$comic->series}} </p>
+                <p> <span class="fw-bold">Sale date:</span> {{$comic->sale_date}} </p>
+                <p> <span class="fw-bold">Type:</span> {{$comic->type}} </p>
+                <a href="" class="btn btn-warning bttn-xs">Edit</a>
+                <a href="" class="btn btn-danger bttn-xs">Delete</a>
             </div>
         </div>
     </div>

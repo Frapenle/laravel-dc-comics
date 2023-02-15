a6f1bfe1d554e709935b5754f450884c756bb6df
francescotrudu <francescotrudu84@gmail.com>
2023-02-15 17:54:39 +0100

    force delete

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -94,7 +94,7 @@ class ComicController extends Controller
     public function destroy($id)
     {
         $comic = Comic::findOrFail($id);
-        $comic->delete();
+        $comic->forceDelete();
         return redirect()->route('admin.comics.index', $comic->id);
     }
 

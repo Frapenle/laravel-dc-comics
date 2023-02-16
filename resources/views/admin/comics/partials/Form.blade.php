5fbbc54d38912b50a9b496d52413f999abb55248
francescotrudu <francescotrudu84@gmail.com>
2023-02-16 20:21:03 +0100

    Message error if not pass validation

--- resources/views/admin/comics/partials/Form.blade.php
+++ resources/views/admin/comics/partials/Form.blade.php
@@ -1,32 +1,52 @@
-<form action="{{route($route, $comic->id)}}" method="POST">
+<form action="{{route($route, $comic->id)}}" method="POST" class="mb-3">
 @csrf
 @method($method)
+{{-- ===== display errors ===== --}}
+    @if ($errors->any())
+        
+    <div class="alert alert-danger">
+        <ul>
+        @foreach ($errors->all() as $error)
+            <li>
+                {{$error}}
+            </li>
+        @endforeach
+        </ul>
+    </div>
+    @endif
     <div class="mb-4">
         <label for="title" class="form-label">Title</label>
-        <input name="title" type="text" class="form-control" id="title" value={{ $comic->title}}>
+        <input name="title" type="text" class="form-control" id="title" value={{ old('title') ?? $comic->title }}>
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
-        <textarea name="description" value="Description" class="form-control" id="description" rows="3">{{ $comic->description}}</textarea>
+        <textarea name="description" value="Description" class="form-control" id="description" rows="3">{{ old('title') ?? $comic->description }}</textarea>
     </div>
     <div class="mb-4">
         <label for="thumb" class="form-label">Link</label>
-        <input name="thumb" type="text" class="form-control" id="thumb" value={{ $comic->thumb}}>
+        <input name="thumb" type="text" class="form-control" id="thumb" value={{ old('title') ?? $comic->thumb }}>
     </div>
     <div class="mb-4">
         <label for="price" class="form-label">Price</label>
-        <input name="price" type="text" class="form-control" id="price" value={{ $comic->price}}>
+        <input name="price" type="text" class="form-control" id="price" value={{ old('title') ?? $comic->price }}>
     </div>
     <div class="mb-4">
         <label for="series" class="form-label">Serie</label>
-        <input name="series" type="text" class="form-control" id="series" value={{ $comic->series}}>
+        <input name="series" type="text" class="form-control" id="series" value={{ old('title') ?? $comic->series }}>
     </div>
     <div class="mb-4">
         <label for="sale_date" class="form-label">Sale date</label>
-        <input name="sale_date" type="text" class="form-control" id="sale_date" value={{ $comic->sale_date}}>
+        <input name="sale_date" type="text" class="form-control" id="sale_date" value={{ old('title') ?? $comic->sale_date }}>
     </div>
     <div class="mb-4">
         <label for="type" class="form-label">Type</label>
-        <input name="type" type="text" class="form-control" id="type" value="{{ $comic->type}}">
+        <input name="type" type="text" class="form-control" id="type" value={{ old('title') ?? $comic->type }}>
     </div>
+    <button type="submit" class="btn 
+    @if ($method == 'DELETE') btn-danger
+    @elseif ($method == 'PUT') btn-warning
+    @else ($method == 'POST') btn-success @endif">
+    {{ $method == 'DELETE' ? 'Elimina' : 'Aggiorna' }}
+    </button>
+
 </form>

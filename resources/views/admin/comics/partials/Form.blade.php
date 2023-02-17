ee5bbce0a5ec1a9026688e6abd6df8b762443222
francescotrudu <francescotrudu84@gmail.com>
2023-02-17 10:07:42 +0100

    Edit old method syntax. Delete nullsafe operator

--- resources/views/admin/comics/partials/Form.blade.php
+++ resources/views/admin/comics/partials/Form.blade.php
@@ -16,31 +16,31 @@
     @endif
     <div class="mb-4">
         <label for="title" class="form-label">Title</label>
-        <input name="title" type="text" class="form-control" id="title" value={{ old('title') ?? $comic->title }}>
+        <input name="title" type="text" class="form-control" id="title" value={{ old('title', $comic->title) }}>
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Description</label>
-        <textarea name="description" value="Description" class="form-control" id="description" rows="3">{{ old('title') ?? $comic->description }}</textarea>
+        <textarea name="description" value="Description" class="form-control" id="description" rows="3">{{ old('title', $comic->description) }}</textarea>
     </div>
     <div class="mb-4">
         <label for="thumb" class="form-label">Link</label>
-        <input name="thumb" type="text" class="form-control" id="thumb" value={{ old('title') ?? $comic->thumb }}>
+        <input name="thumb" type="text" class="form-control" id="thumb" value={{ old('title', $comic->thumb) }}>
     </div>
     <div class="mb-4">
         <label for="price" class="form-label">Price</label>
-        <input name="price" type="text" class="form-control" id="price" value={{ old('title') ?? $comic->price }}>
+        <input name="price" type="text" class="form-control" id="price" value={{ old('title', $comic->price) }}>
     </div>
     <div class="mb-4">
         <label for="series" class="form-label">Serie</label>
-        <input name="series" type="text" class="form-control" id="series" value={{ old('title') ?? $comic->series }}>
+        <input name="series" type="text" class="form-control" id="series" value={{ old('title', $comic->series) }}>
     </div>
     <div class="mb-4">
         <label for="sale_date" class="form-label">Sale date</label>
-        <input name="sale_date" type="text" class="form-control" id="sale_date" value={{ old('title') ?? $comic->sale_date }}>
+        <input name="sale_date" type="text" class="form-control" id="sale_date" value={{ old('title', $comic->sale_date) }}>
     </div>
     <div class="mb-4">
         <label for="type" class="form-label">Type</label>
-        <input name="type" type="text" class="form-control" id="type" value={{ old('title') ?? $comic->type }}>
+        <input name="type" type="text" class="form-control" id="type" value={{ old('title', $comic->type) }}>
     </div>
     <button type="submit" class="btn {{ $method == 'PUT' ? 'btn-warning' : 'btn-success' }}">
     {{ $method == 'PUT' ? 'Modifica' : 'Aggiungi' }}
